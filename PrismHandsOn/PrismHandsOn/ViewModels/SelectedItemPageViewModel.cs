d97dfea1e6bc2c395a8f503791fae88afb110060
gnk263 <gnk263@users.noreply.github.com>
2018-08-25 16:01:03 +0900

    遷移先の画面を作成する

---
+++ PrismHandsOn/PrismHandsOn/ViewModels/SelectedItemPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using Prism.Mvvm;
+using Xamarin.Forms;
+
+namespace PrismHandsOn.ViewModels
+{
+    public class SelectedItemPageViewModel : BindableBase
+    {
+        private string _colorName;
+
+        private Color _color;
+
+        public string ColorName
+        {
+            get => _colorName;
+            set => SetProperty(ref _colorName, value);
+        }
+
+        public Color Color
+        {
+            get => _color;
+            set => SetProperty(ref _color, value);
+        }
+    }
+}
