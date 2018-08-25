c56b0e6518e568a4aae163c217316f796b3cae7e
gnk263 <gnk263@users.noreply.github.com>
2018-08-25 14:52:34 +0900

    MainPageViewModelのMessageプロパティの変更通知を実装する

--- PrismHandsOn/PrismHandsOn/ViewModels/MainPageViewModel.cs
+++ PrismHandsOn/PrismHandsOn/ViewModels/MainPageViewModel.cs
@@ -5,7 +5,12 @@ namespace PrismHandsOn.ViewModels
 {
     public class MainPageViewModel : BindableBase
     {
-        public string Message { get; } = "Hello, Prism for Xamarin.Forms!";
+        public string _message = "Hello, Prism for Xamarin.Forms!";
+        public string Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
+        }
 
         public MainPageViewModel()
         {
