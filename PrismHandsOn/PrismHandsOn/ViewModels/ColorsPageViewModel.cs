617947f97d2fe287abf54d3849dda3e5da0c4755
gnk263 <gnk263@users.noreply.github.com>
2018-08-25 15:50:58 +0900

    ColorsPageに色の一覧を表示する

---
+++ PrismHandsOn/PrismHandsOn/ViewModels/ColorsPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace PrismHandsOn.ViewModels
+{
+    public class ColorsPageViewModel
+    {
+        public IReadOnlyCollection<ColorInfo> ColorInfos { get; } =
+            typeof(Color)
+                .GetRuntimeFields()
+                .Where(x => x.IsPublic && x.IsStatic && x.FieldType == typeof(Color))
+                .Select(x => new ColorInfo { Name = x.Name, Color = (Color)x.GetValue(null) })
+                .ToList();
+    }
+}
